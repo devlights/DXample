0b0afde96dccd1a1f9f4e0c45dabaf3ff033d016
devlights <gsf.zero1@gmail.com>
2013-05-21 10:51:18 +0900

    code clearning

--- src/ORM/XPOSample02/Program.cs
+++ src/ORM/XPOSample02/Program.cs
@@ -1,14 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace XPOSample02
+﻿namespace XPOSample02
 {
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+
+  using DevExpress.Xpo;
+  
   class Program
   {
-    static void Main(string[] args)
+    static void Main()
     {
     }
   }
