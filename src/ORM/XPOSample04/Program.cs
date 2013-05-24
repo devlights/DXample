d351857a552dc4b8daa0cc749865c007cb3bf2a0
devlights <gsf.zero1@gmail.com>
2013-05-24 15:23:34 +0900

    add XPO reference

--- src/ORM/XPOSample04/Program.cs
+++ src/ORM/XPOSample04/Program.cs
@@ -1,14 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace XPOSample04
 {
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+
+  using DevExpress.Xpo;
+  using DevExpress.Xpo.DB;
+
   class Program
   {
-    static void Main(string[] args)
+    static void Main()
     {
     }
   }
