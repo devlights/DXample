728beb10dc4de6e538d29fc4daa69fe5034cd31a
devlights <gsf.zero1@gmail.com>
2013-06-15 17:22:12 +0900

    implement (fix #6)

---
+++ src/ORM/XPOSample06/Models/Customer.cs
@@ -0,0 +1,42 @@
+﻿namespace DXample.XPOSample06.Models
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+
+  using DevExpress.Xpo;
+
+  public class Customer : XPObject
+  {
+    private int _age;
+    private string _name;
+
+    public Customer(Session sess) : base(sess)
+    {
+    }
+
+    public string Name
+    {
+      get
+      {
+        return _name;
+      }
+      set
+      {
+        SetPropertyValue("Name", ref _name, value);
+      }
+    }
+
+    public int Age
+    {
+      get
+      {
+        return _age;
+      }
+      set
+      {
+        SetPropertyValue("Age", ref _age, value);
+      }
+    }
+  }
+}
