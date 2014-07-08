8d025d3cb24a6b396087bfd2b521f15c1ecbb86a
devlights <gsf.zero1@devlights.onmicrosoft.com>
2014-07-08 16:55:53 +0900

    テストケース追加

---
+++ src/Server/DXample.Server.Tests/DataServiceTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DXample.Server.Interfaces;
+using NUnit.Framework;
+
+namespace DXample.Server.Tests
+{
+    [TestFixture]
+    class DataServiceTest
+    {
+        [TestCase(1)]
+        [TestCase(100)]
+        [TestCase(1000)]
+        [TestCase(10000)]
+        [TestCase(100000)]
+        [TestCase(1000000)]
+        public void GetAllEmployees_データが取得できる(int numberOfEmployees)
+        {
+            // Arrange
+            DataServiceState.Instance.NumberOfEmployees = numberOfEmployees;
+
+            var sut = new DataService();
+
+            // Act
+            var results = sut.GetAllEmployees();
+
+            // Assert
+            Assert.That(() => results.Count(), Is.EqualTo(numberOfEmployees));
+
+            var item = results.First();
+            Assert.That(() => item.EmployeeType, Is.EqualTo(EmployeeType.Developer));
+            Assert.That(() => item.Name, Is.EqualTo("Employee1"));
+            Assert.That(() => item.Age, Is.EqualTo(21));
+            Assert.That(() => item.Salary, Is.GreaterThanOrEqualTo(200000));
+        }
+    }
+}
