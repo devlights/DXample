3364bf5e525c29edf3cdfb508048b62c2ede9ceb
devlights <gsf.zero1@devlights.onmicrosoft.com>
2014-07-08 17:33:42 +0900

    SimpleButtonのサンプルプロジェクト追加

---
+++ src/WinForms/DXample.WinForms.SimpleButtonSample/MainForm.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace DXample.WinForms.SimpleButtonSample
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DevExpress.Utils.SuperToolTip superToolTip1 = new DevExpress.Utils.SuperToolTip();
+            DevExpress.Utils.ToolTipTitleItem toolTipTitleItem1 = new DevExpress.Utils.ToolTipTitleItem();
+            DevExpress.Utils.ToolTipItem toolTipItem1 = new DevExpress.Utils.ToolTipItem();
+            DevExpress.Utils.ToolTipSeparatorItem toolTipSeparatorItem1 = new DevExpress.Utils.ToolTipSeparatorItem();
+            DevExpress.Utils.ToolTipTitleItem toolTipTitleItem2 = new DevExpress.Utils.ToolTipTitleItem();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            this.SuspendLayout();
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(93, 12);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(120, 23);
+            this.simpleButton1.TabIndex = 0;
+            this.simpleButton1.Text = "デフォルト状態のボタン";
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Location = new System.Drawing.Point(12, 17);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(74, 13);
+            this.labelControl1.TabIndex = 1;
+            this.labelControl1.Text = "デフォルト状態：";
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Location = new System.Drawing.Point(12, 55);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(237, 13);
+            this.labelControl2.TabIndex = 2;
+            this.labelControl2.Text = "AllowHtmlDrawをTrueにするとTextでHtmlが使える：";
+            // 
+            // simpleButton2
+            // 
+            this.simpleButton2.AllowHtmlDraw = DevExpress.Utils.DefaultBoolean.True;
+            this.simpleButton2.Location = new System.Drawing.Point(12, 74);
+            this.simpleButton2.Name = "simpleButton2";
+            this.simpleButton2.Size = new System.Drawing.Size(339, 23);
+            toolTipTitleItem1.Text = "利用できるHTMLタグ";
+            toolTipItem1.LeftIndent = 6;
+            toolTipItem1.Text = "利用できるHTMLのタグは決まっている。利用不可のものを記述するとボタンのキャプションにそのまま表示される。";
+            toolTipTitleItem2.LeftIndent = 6;
+            toolTipTitleItem2.Text = "DevExpress HTML Text Formatting でGoogle検索";
+            superToolTip1.Items.Add(toolTipTitleItem1);
+            superToolTip1.Items.Add(toolTipItem1);
+            superToolTip1.Items.Add(toolTipSeparatorItem1);
+            superToolTip1.Items.Add(toolTipTitleItem2);
+            this.simpleButton2.SuperTip = superToolTip1;
+            this.simpleButton2.TabIndex = 3;
+            this.simpleButton2.Text = "<i><b><color=red><backcolor=blue>太字で赤色で背景青状態</backcolor></color></b></i>";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(363, 278);
+            this.Controls.Add(this.simpleButton2);
+            this.Controls.Add(this.labelControl2);
+            this.Controls.Add(this.labelControl1);
+            this.Controls.Add(this.simpleButton1);
+            this.Name = "MainForm";
+            this.Text = "SimpleButton Sample";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.SimpleButton simpleButton2;
+
+    }
+}
+
