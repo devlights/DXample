a51d233ef6b3c0ff3ce3d60d782fcc9cb19ee4f1
devlights <gsf.zero1@devlights.onmicrosoft.com>
2014-07-08 15:05:12 +0900

    ViewModel 調整

--- src/Server/DXample.Server.UI/MainViewModel.cs
+++ src/Server/DXample.Server.UI/MainViewModel.cs
@@ -1,21 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.ServiceModel;
-using System.Text;
 using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using DevExpress.Mvvm;
+
 using DevExpress.Mvvm.POCO;
-using DevExpress.Xpf.Core;
 
 namespace DXample.Server.UI
 {
@@ -23,7 +11,7 @@ namespace DXample.Server.UI
     {
         public MainViewModel()
         {
-            NumberOfEmployees = 350;
+            NumberOfEmployees = 100;
         }
 
         public static MainViewModel Create()
